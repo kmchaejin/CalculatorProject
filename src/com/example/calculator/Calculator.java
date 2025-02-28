00cbcdad56396c0954daf97379d526f85b924bd3
t2023-m0082 <t2023-m0082@t2023-m0082ui-MacBookAir.local>
2025-02-28 15:39:25 +0900

    기본 계산기 구현

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,7 +1,35 @@
 package com.example.calculator;
+import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("test");
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        char calc = scanner.next().charAt(0);
+        double result = 0.0;
+
+        switch(calc){
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = (double) num1 / num2;
+                break;
+            case '%':
+                result = num1 % num2;
+                break;
+            default:
+                System.out.println("잘못된 연산 기호입니다.");
+                break;
+        }
+
+        System.out.println("" + num1 + calc + num2 + "=" + result);
     }
 }

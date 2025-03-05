205bfd8c9a3df5f05572830e8c8a2b8b2a075f9c
t2023-m0082 <t2023-m0082@t2023-m0082ui-MacBookAir.local>
2025-03-05 14:13:30 +0900

    제네릭 적용

--- src/com/example/calculator/App.java
+++ src/com/example/calculator/App.java
@@ -8,12 +8,15 @@ public class App {
         Calculator calculator = new Calculator();
 
         while (true) {
-            int num1 = 0;
-            double result = 0.0;
+            String firstInput="";
+            String secondInput="";
+            char operator=' ';
+            double doubleNum=0.0;
+            int intNum=0;
 
             // 첫 번째 값 입력
             System.out.println("첫 번째 숫자를 입력해주세요. (exit 입력 시 종료됩니다)");
-            String firstInput = scanner.next();
+            firstInput = scanner.next();
 
             // 입력값이 exit면 종료
             if (firstInput.equals("exit")) {
@@ -21,30 +24,58 @@ public class App {
                 break;
             } else {
                 try {
-                    // 입력한 숫자의 타입을 정수로 변환
-                    num1 = Integer.parseInt(firstInput);
+                    // 입력한 숫자의 타입을 실수로 변환
+                    if(firstInput.contains(".")){
+                        doubleNum = Double.parseDouble(firstInput);
+                        calculator.setNum1(doubleNum);
+                    }
+                    else {
+                        // 입력한 숫자의 타입을 정수로 변환
+                        intNum = Integer.parseInt(firstInput);
+                        calculator.setNum1(intNum);
+                    }
 
                 } catch (NumberFormatException e) {
                     // exit 외의 문자열을 입력한 경우 재입력
-                    System.out.println("exit 또는 양의 정수만 입력해주세요.");
+                    System.out.println("exit 또는 숫자만 입력해주세요.");
                     continue;
                 }
             }
 
             // 두 번째 정수와 연산 기호 입력
-            System.out.println("두 번째 숫자를 입력해주세요.");
-            int num2 = scanner.nextInt();
-            System.out.println("연산 기호를 입력해주세요.");
-            char calc = scanner.next().charAt(0);
+            System.out.println("두 번째 숫자를 입력해주세요. (exit 입력 시 종료됩니다)");
+            secondInput = scanner.next();
 
-            // 입력값이 음수이면 재입력
-            if (num1 < 0 || num2 < 0) {
-                System.out.println("양의 정수를 입력해주세요.");
+            if (secondInput.equals("exit")) {
+                System.out.println("프로그램이 종료됩니다.");
+                break;
+            } else {
+                try {
+                    // 입력한 숫자의 타입을 실수로 변환
+                    if(secondInput.contains(".")){
+                        doubleNum = Double.parseDouble(secondInput);
+                        calculator.setNum2(doubleNum);
+                    }
+                    else {
+                        // 입력한 숫자의 타입을 정수로 변환
+                        intNum = Integer.parseInt(secondInput);
+                        calculator.setNum2(intNum);
+                    }
+
+                } catch (NumberFormatException e) {
+                    // exit 외의 문자열을 입력한 경우 재입력
+                    System.out.println("exit 또는 숫자만 입력해주세요.");
+                    continue;
+                }
             }
 
-            // 계산하는 메서드 호출
-            result = calculator.calculate(num1, num2, calc);
-            System.out.println(num1 + " " + calc + " " + num2 + " " + "=" + " " + result + "\n");
+            System.out.println("연산 기호를 입력해주세요.");
+            operator = scanner.next().charAt(0);
+            calculator.setSymbol(operator);
+
+            calculator.calculate();
+
+            System.out.println(firstInput + " " + operator + " " + secondInput + " = " + calculator.getLastestResult() + "\n");
         }
 
         // 연산 결과 리스트 출력
--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -3,12 +3,27 @@ package com.example.calculator;
 import java.util.List;
 import java.util.ArrayList;
 
-public class Calculator {
-    double result = 0.0;
+public class Calculator<T extends Number> {
+    double result;
+    double num1;
+    double num2;
+    char operator;
     private List<Double> resultList = new ArrayList<>();
 
-    public double calculate(int num1, int num2, char calc){
-        switch (calc) {
+    public void setNum1(T num1){
+        this.num1 = num1.doubleValue();
+    }
+
+    public void setNum2(T num2){
+        this.num2 = num2.doubleValue();
+    }
+
+    public void setSymbol(char operator){
+        this.operator = operator;
+    }
+
+    public void calculate(){
+        switch (operator) {
             case '+':
                 result = num1 + num2;
                 break;
@@ -19,7 +34,7 @@ public class Calculator {
                 result = num1 * num2;
                 break;
             case '/':
-                result = (double) num1 / num2;
+                result = num1 / num2;
                 break;
             case '%':
                 result = num1 % num2;
@@ -30,7 +45,9 @@ public class Calculator {
         }
 
         resultList.add(result);
-        return result;
+    }
+    public Double getLastestResult(){
+        return resultList.get(resultList.size()-1);
     }
 
     public void removeFirstData(){
@@ -41,7 +58,7 @@ public class Calculator {
         return resultList;
     }
 
-    public void setResult(double num){
-        resultList.set(0, num);
+    public void setResult(T num){
+        resultList.set(0, num.doubleValue());
     }
 }
